d6124c0c93ca978b2c570589e2fd7c7a26b7e846
Dhruv Dugar <dhruvdugar@Dhruvs-MacBook-Pro.local>
2024-11-13 12:40:33 +0530

    fixed h2 not initialising

--- productApplication/src/main/java/com/Product/productApplication/Service/ProductServiceImplementation.java
+++ productApplication/src/main/java/com/Product/productApplication/Service/ProductServiceImplementation.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ProductServiceImplementation {
+public class ProductServiceImplementation implements ProductService{
 	private final ProductRepository pRepo;
 
 	@Autowired
